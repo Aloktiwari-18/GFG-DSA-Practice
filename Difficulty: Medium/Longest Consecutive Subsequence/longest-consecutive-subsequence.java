2c8d505b4ef67658bae0009d990a855b621a8568
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-01-27 12:15:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int longestConsecutive(int[] arr) {
+        int res=0;
+        int n= arr.length;
+        int count=0;
+        HashSet<Integer> set= new HashSet<>();
+        
+        for(int val:arr){
+            set.add(val);
+        }
+        
+        
+        for(int val:set){
+            if(set.contains(val) && !set.contains(val-1)){
+                int curr=val;
+                count=0;
+                
+                while(set.contains(curr)){
+                    set.remove(curr);
+                    curr++;
+                    count++;
+                }
+                res= Math.max(res,count);
+                
+            }
+            
+        }
+        return res;
+
+
+        
+    }
+}
