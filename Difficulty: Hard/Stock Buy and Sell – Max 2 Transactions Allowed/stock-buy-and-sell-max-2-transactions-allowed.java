bbada2c7e5ab8ed8b20e3bc42b2511d8b57686cb
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-02-01 18:52:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.java
+++ Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.java
@@ -1,20 +1,24 @@
 class Solution {
     public static int maxProfit(int[] prices) {
-        // code here
-        int firstBuy=Integer.MIN_VALUE;
-        int firstSell= 0;
+        int firstBuy= Integer.MIN_VALUE;
+        int firstSell=0;
+        
         int secondBuy= Integer.MIN_VALUE;
         int secondSell= 0;
         int n= prices.length;
         
-        for(int i=0;i<n;i++){
+        for(int i=0 ;i<n;i++){
             firstBuy=Math.max(firstBuy, -prices[i]);
-            firstSell= Math.max(firstSell, prices[i]+firstBuy);
-            secondBuy= Math.max(secondBuy, firstSell-prices[i]);
-            secondSell= Math.max(secondSell, prices[i]+secondBuy);
+            
+            firstSell=Math.max(firstSell,prices[i]+firstBuy);
+            
+            secondBuy=Math.max(secondBuy,firstSell-prices[i]);
+            
+            secondSell=Math.max(secondSell,  prices[i]+secondBuy);
             
         }
         return secondSell;
+        // code here
         
         
     }
