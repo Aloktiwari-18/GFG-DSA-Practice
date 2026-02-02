49b64e9728460db0487b88203695f39f6778cb2a
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-02-02 11:24:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST to max heap/bst-to-max-heap.java
@@ -0,0 +1,43 @@
+// User function Template for Java
+
+/*class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}*/
+
+class Solution {
+    
+    static int idx;
+    public static void postOrder(Node root, ArrayList<Integer> in){
+        if(root==null) return ;
+        postOrder(root.left,in);
+        postOrder(root.right,in);
+        
+        root.data=in.get(idx++);
+    }
+    public static void inOrder(Node root, ArrayList<Integer> in){
+        if(root==null) return ;
+        inOrder(root.left,in);
+        in.add(root.data);
+        inOrder(root.right,in);
+    }
+    
+    
+    public static void convertToMaxHeapUtil(Node root) {
+        idx=0;
+        ArrayList<Integer> in= new ArrayList<>();
+        
+        inOrder(root,in);
+        postOrder(root,in);
+        
+        
+        
+        
+    }
+}
