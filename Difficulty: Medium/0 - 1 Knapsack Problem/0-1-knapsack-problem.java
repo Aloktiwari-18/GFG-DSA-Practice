5551f244b53283263a6e37f09f78de943ddcf7ef
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-01 23:35:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int profit(int i ,int [] wt,int []val,int C, int [][] dp){
+        if(i==wt.length) return 0;
+        if(dp[i][C]!=-1){
+            return dp[i][C];
+        }
+        int skip= profit(i+1, wt, val,C, dp);
+        if(wt[i]>C) return dp[i][C]= skip;
+        int pick= val[i]+ profit(i+1, wt,val,C-wt[i],dp);
+        return   dp[i][C]= Math.max(pick,skip);
+    }
+    public int knapsack(int W, int val[], int wt[]) {
+        int n=wt.length;
+        
+        int dp[][]=new int[n][W+1]; 
+        for(int i=0;i<n;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]= -1;
+            }
+        }
+        return profit(0,wt,val,W,dp);
+        
+        
+    }
+}
