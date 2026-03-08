23118c17ec5bf798650c005ce05fc773b6b40cb3
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-03-08 18:21:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -10,52 +10,51 @@ class Node {
     }
 }
 */
-
 class Pair{
-    Node node;
     int hd;
+    Node temp;
     
-    Pair(Node _node, int _hd){
-        this.node= _node;
-        this.hd= _hd;
+    Pair(Node _temp,int _hd){
+        this.hd=_hd;
+        this.temp= _temp;
     }
 }
+
 class Solution {
-    
     public ArrayList<Integer> topView(Node root) {
+        // code here
         Queue<Pair> q= new LinkedList<>();
+        
+        Map<Integer,Integer> map= new TreeMap<>();
         ArrayList<Integer> ans= new ArrayList<>();
-        TreeMap<Integer, Integer> map= new TreeMap<>();
         if(root==null) return ans;
-        
         q.add(new Pair(root,0));
-        
         while(!q.isEmpty()){
             Pair it= q.poll();
+            
             int hd= it.hd;
-            Node temp= it.node;
+            Node temp= it.temp;
+            
             
             if(map.get(hd)==null){
                 map.put(hd, temp.data);
             }
             if(temp.left!=null){
-                q.add(new Pair(temp.left,hd-1));
+                q.add(new Pair(temp.left, hd-1));
+               
+                
             }
-            
             if(temp.right!=null){
-                q.add(new Pair(temp.right,hd+1));
-            }            
-            
-            
+                q.add(new Pair(temp.right, hd+1));
+                
+            }
+
             
         }
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
             ans.add(entry.getValue());
         }
         return ans;
-        
-        
-        // code here
-        
     }
+    
 }
