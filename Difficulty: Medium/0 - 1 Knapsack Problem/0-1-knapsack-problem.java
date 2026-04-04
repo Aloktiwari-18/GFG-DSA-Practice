9105a2e72381739afe466d58cf0dc4673964b075
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-04 17:40:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,40 +1,29 @@
 class Solution {
-    // public static int profit(int i ,int [] wt,int []val,int C, int [][] dp){
-    //     if(i==wt.length) return 0;
-    //     if(dp[i][C]!=-1){
-    //         return dp[i][C];
-    //     }
-    //     int skip= profit(i-1, wt, val,C, dp);
-    //     // if(wt[i]>C) return dp[i][C]= skip;
-    //     int pick= val[i]+ profit(i-1, wt,val,C-wt[i],dp);
-    //     // return   dp[i][C]= Math.max(pick,skip);
-    // }
-    public int knapsack(int W, int val[], int wt[]) {
-        int n=wt.length;
-        
-        int dp[][]=new int[n][W+1]; 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<W+1;j++){
-                int skip= (i>0)? dp[i-1][j]:0;
-                
-                if(wt[i]>j)  dp[i][j]=skip;
-                
-                else{
-                    int pick= val[i];
-                    pick+=((i>0)? dp[i-1][j-wt[i]]:0);
-                    
-                    dp[i][j]=Math.max(pick, skip);
-                }
-                
-                
-                
+    public static int calculate(int i, int W, int [] val,int []wt,int [][]dp ){
+       if(i==wt.length) return 0;
+        if(dp[i][W]!=-1){
+            return dp[i][W];
+
         }
-       
-        
+        int skip= calculate(i+1, W, val, wt, dp);
+        if(wt[i]>W){
+            return dp[i][W]= skip;
+        }
+        int take= val[i]+calculate(i+1, W-wt[i], val, wt, dp);
+        return dp[i][W]=Math.max(take, skip);
         
     }
-     return dp[n-1][W];
-}
+    public int knapsack(int W, int val[], int wt[]) {
+        int n= wt.length;
 
+        // code here
+        int dp[][]= new int [n][W+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<W+1;j++){
+                dp[i][j]= -1;
+            }
+        }
+        return calculate(0,W, val, wt, dp );
+        
+    }
 }
-
