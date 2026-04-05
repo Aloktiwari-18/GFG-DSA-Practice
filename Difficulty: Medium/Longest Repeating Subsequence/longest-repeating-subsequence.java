d4d85ad6649101a18c12b2c617ecdfd6da13bfea
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-05 18:59:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.java
+++ Difficulty: Medium/Longest Repeating Subsequence/longest-repeating-subsequence.java
@@ -1,39 +1,29 @@
+// User function Template for Java
+
 class Solution {
-
-    public int solve(int i, int j, String s, int[][] dp){
-
-        if(i < 0 || j < 0){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
+    public static int calculate(int i, int j, StringBuilder sb, int [][] dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1){
             return dp[i][j];
+            
         }
-
-        if(s.charAt(i) == s.charAt(j) && i != j){
-            dp[i][j] = 1 + solve(i-1, j-1, s, dp);
+        if(sb.charAt(i)==sb.charAt(j) && i!=j){
+            dp[i][j]=1+calculate(i-1, j-1, sb, dp);
+        }else{
+            dp[i][j]=Math.max(calculate(i-1,j,sb, dp), calculate(i, j-1, sb,dp));
         }
-        else{
-            dp[i][j] = Math.max(
-                solve(i-1, j, s, dp),
-                solve(i, j-1, s, dp)
-            );
-        }
-
         return dp[i][j];
-    }
-
+    } 
     public int LongestRepeatingSubsequence(String s) {
-
-        int n = s.length();
-        int[][] dp = new int[n][n];
-
+        // code here
+        StringBuilder sb= new StringBuilder (s);
+        int n= sb.length();
+        int dp[][]= new int[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                dp[i][j] = -1;
+                dp[i][j]=-1;
             }
         }
-
-        return solve(n-1, n-1, s, dp);
+        return calculate(n-1, n-1, sb, dp);
     }
 }
