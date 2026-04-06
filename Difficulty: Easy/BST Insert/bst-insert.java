c266aef95297ddb59448307576b2c1048d86c31c
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-04-07 02:09:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BST Insert/bst-insert.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public Node insert(Node root, int key) {
+        // code here
+        if(root==null) {
+            Node ans= new Node(key);
+            return ans;
+        }
+        if(root.data>key){
+            root.left= insert(root.left, key);
+            
+            
+        }else{
+            root.right= insert(root.right, key);
+        }
+        return root;
+    }
+}
