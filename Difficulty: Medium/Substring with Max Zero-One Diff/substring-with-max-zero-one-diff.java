65caeed379e70e981dc5aa981d824cf6951d08f2
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-05-23 03:24:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Substring with Max Zero-One Diff/substring-with-max-zero-one-diff.java
@@ -0,0 +1,17 @@
+class Solution {
+    int maxSubstring(String s) {
+        // code here
+         int n= s.length();
+         int prefix= s.charAt(0)=='0' ?1:-1;
+         int ans= prefix;
+         for(int i=1;i<n;i++){
+             int val= s.charAt(i)=='0' ?1:-1;
+             prefix= Math.max(prefix+val, val);
+             ans= Math.max(prefix, ans);
+             
+         }
+         return ans;
+         
+         
+    }
+}
