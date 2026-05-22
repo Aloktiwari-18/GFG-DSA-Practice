706aeae226369a6b52b9ec8a207ac8219c49a65d
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-05-23 02:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.java
@@ -0,0 +1,14 @@
+class Solution {
+    static boolean isPerfect(int n) {
+        // code here
+        int sum=1;
+        for(int i=2;i*i<n;i++){
+            if(n%i==0){
+                sum+=i;
+                sum+=n/i;
+            }
+        }
+        if(sum==n) return true;
+        return false;
+    }
+};
