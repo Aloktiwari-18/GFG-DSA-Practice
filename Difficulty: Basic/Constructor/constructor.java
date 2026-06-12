6c2a2e87a526eaef3305b47dea0dc370ef9864a4
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-06-12 14:43:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Constructor/constructor.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+// design a class User
+// instance variable: name(String)
+// Constructor: no parameter, initialize name to "Default"
+
+// code here
+
+class User {
+    String name;
+
+    // Constructor
+    public User(){
+        this.name= "Default";
+    }
+    }
