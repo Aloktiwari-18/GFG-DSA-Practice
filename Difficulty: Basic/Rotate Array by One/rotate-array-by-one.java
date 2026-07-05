777b6f83c2bcec7fcb7c85a81b89b5034fb1971f
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-06 03:08:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
@@ -1,20 +1,15 @@
-// // User function Template for Java
-
 class Solution {
-    public static void swap(int arr[], int i, int j){
-        int temp= arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
     public void rotate(int[] arr) {
         // code here
-        int end= arr.length-1;
-        int secEnd= arr.length-2;
-        while(secEnd>=0){
-
-            swap(arr, secEnd, end);
-            secEnd--;
-            end--;
+        int n= arr.length;
+        int res[]= new int[n];
+        res[0]=arr[n-1];
+        int k=1;
+        for(int i=0;i<n-1;i++){
+            res[k++]=arr[i];
+        }
+        for(int i=0;i<n;i++){
+            arr[i]=res[i];
         }
     }
 }
