a92d95b9926a917e4e82f73a7a49c810ce3e7e9f
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-07-25 18:17:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Doubly Linked List from an Array/doubly-linked-list-from-an-array.java
@@ -0,0 +1,30 @@
+/* class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int d) {
+        data = d;
+        next = null;
+        prev = null;
+    }
+} */
+
+class Solution {
+    public Node createDLL(int arr[]) {
+        // code here
+       
+        int n=arr.length;
+        Node head = new Node(arr[0]);
+        Node prev=head;
+        for(int i=1;i<n;i++){
+            Node temp= new Node(arr[i]);
+            prev.next= temp;
+            temp.prev=prev;
+            prev=temp;
+            
+            
+        }
+        return head;
+    }
+}
