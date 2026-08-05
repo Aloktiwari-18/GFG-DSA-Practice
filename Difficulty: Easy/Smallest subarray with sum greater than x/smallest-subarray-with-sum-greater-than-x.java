c591bae8e642a1ccb25ec17fdfbb5097909fc4f3
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-06 00:16:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
@@ -1,27 +1,25 @@
 class Solution {
     public static int smallestSubWithSum(int x, int[] arr) {
-        int n= arr.length;
+        // Your code goes here
         int left=0;
+        int n= arr.length;
+        int min= Integer.MAX_VALUE;
         int right=0;
-        int count=0;
-        int minCount=Integer.MAX_VALUE;
         int sum=0;
         while(right<n){
             sum+=arr[right];
             while(sum>x){
-                count=right-left+1;
-                minCount=  Math.min(count, minCount);
-                 sum-=arr[left];
+                int count=right-left+1;
+                min= Math.min(count, min);
+                sum-=arr[left];
                 left++;
-               
-                
             }
             right++;
         }
-        
-        if(minCount==Integer.MAX_VALUE){
+        if(min==Integer.MAX_VALUE){
             return 0;
+        }else{
+            return min;
         }
-        return minCount;
     }
 }
