046095584e3d676993a25e964a5eb7d5140215bb
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-08 20:22:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Subarray Sum by Removing At Most One/max-subarray-sum-by-removing-at-most-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+    public int maxSumSubarray(int[] arr) {
+        // code here
+        int noDel=arr[0];
+        int oneDel= 0;
+        int max=arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            
+            oneDel=Math.max(noDel,arr[i]+oneDel);
+            noDel= Math.max(arr[i], noDel+arr[i]);
+            max=Math.max(max, Math.max(noDel, oneDel));
+        }
+        
+        return max;
+    }
+}
