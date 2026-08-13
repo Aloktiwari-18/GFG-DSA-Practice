50f04954695abb6a8ab21c4ab7d3a414becc27a6
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-13 14:02:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int x=a;
+        int y=b;
+        while(y!=0){
+            int r= x%y;
+            x=y;
+            y=r;
+        }
+        int gcd= x;
+        int lcm= (a*b)/gcd;
+        int ans[]= new int[]{lcm, gcd};
+        return ans;
+    }
+}
