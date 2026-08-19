c62f6cdf60d206fe07b17b38a637a4d6a6c1a7e1
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-19 21:02:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -1,23 +1,23 @@
 class Solution {
     static boolean armstrongNumber(int n) {
-        // code here
         int count=0;
-        int num=n;
-        while(num>0){
-            num=num/10;
+        int real=n;
+        while(real>0){
             count++;
+            real=real/10;
         }
         int sum=0;
-        int act=n;
-        while(act>0){
-            int rem= act%10;
+        int num=n;
+        while(num>0){
+            int rem= num%10;
             sum+=Math.pow(rem, count);
-            act=act/10;
-            
+            num=num/10;
         }
-        if(n==sum){
+        if(sum==n){
             return true;
         }
+        // code here
         return false;
+        
     }
 }
