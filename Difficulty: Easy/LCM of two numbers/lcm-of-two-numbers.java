56d8f69148a9e2173bcad2a7c983f6a7a9f3d761
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-19 21:20:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/LCM of two numbers/lcm-of-two-numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    int gcd(int a, int b){
+        return (b==0)? a: gcd(b, a%b);
+    }
+    
+    public int lcm(int a, int b) {
+        // code here
+        
+        return (a/gcd(a, b))*b;
+    }
+}
