98ac953e565114aca1e25c0e4e996791fdad39cd
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-24 15:27:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
@@ -1,28 +1,28 @@
 class Solution {
-    public static int calculate(int coins[], int n, int sum, int [][]dp){
-        if(sum==0) return 1;
-        if(n==0 || sum< 0){
+    
+    public int solve(int [] coins, int idx, int sum, int target, int dp[][]){
+        if(sum==target){
+            return 1;
+        }
+        
+        
+        if(idx==coins.length || sum>target){
             return 0;
         }
-        if(dp[n][sum]!=-1){
-            return dp[n][sum];
+        if(dp[idx][sum]!=-1){
+            return dp[idx][sum];
         }
-        return dp[n][sum]= calculate(coins, n-1, sum, dp)+
-        calculate(coins, n, sum-coins[n-1],dp);
-
-    }
-    public int count(int coins[], int sum) {
-        int n= coins.length;
-
-        // code here.
+        return dp[idx][sum]=solve(coins, idx+1, sum, target, dp )+ solve(coins, idx, sum+coins[idx], target , dp);
         
-        int dp[][]= new int [n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                dp[i][j]=-1;
-            }
+    }
+    public int count(int coins[], int target) {
+        
+        int[][] dp = new int[coins.length][target + 1];
             
+        for(int []row: dp){
+            Arrays.fill(row, -1);
         }
-        return calculate(coins, n, sum, dp);
+        // code here.
+        return solve(coins, 0,  0, target, dp);
     }
 }
