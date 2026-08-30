5180faefb151d4090b8a9b6f7083b8b347af4b60
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-30 13:22:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with Distinct Characters/longest-substring-with-distinct-characters.java
+++ Difficulty: Medium/Longest Substring with Distinct Characters/longest-substring-with-distinct-characters.java
@@ -2,18 +2,16 @@ class Solution {
     public int longestUniqueSubstr(String s) {
         // code here
         HashSet<Character> set= new HashSet<>();
-        int right=0;
         int left=0;
-        int ans=Integer.MIN_VALUE;
+        int right=0;
+        int ans= Integer.MIN_VALUE;
         while(right<s.length()){
-            char ch= s.charAt(right);
-            while(set.contains(ch)){
-                
+            while(set.contains(s.charAt(right))){
                 set.remove(s.charAt(left));
                 left++;
             }
-            ans=Math.max(ans, right-left+1);
-            set.add(ch);
+            set.add(s.charAt(right));
+            ans=Math.max(right-left+1,ans);
             right++;
         }
         return ans;
