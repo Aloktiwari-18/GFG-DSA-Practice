d45101699cf68ba9999831f6c98e67327e1b0476
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-08-30 23:24:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,28 +1,27 @@
 class Solution {
-    public static boolean sameType(char top, char ch){
-        if(top=='[' && ch==']') return true;
-        if(top=='{' && ch=='}') return true;
-        if(top=='(' && ch==')') return true;
-        else return false;
-    }
     public boolean isBalanced(String s) {
         // code here
         Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch= s.charAt(i);
-
-          
-           if(ch=='(' || ch== '{' || ch=='[') st.push(ch);
-           else{
-               if(st.size()==0){
-                   return false;
-               }
-               char top= st.peek();
-               if(sameType(top, ch)) st.pop();
-               else return false;
-           }
+            if(st.size()>0 && (st.peek()=='(' && ch==')'   || st.peek()=='{' && ch=='}'  || st.peek()=='[' && ch==']' )){
+                st.pop();
+            }
+            else if(st.size()==0){
+                if(ch=='}' || ch==']' || ch==')'){
+                    return false;
+                }else{
+                    st.push(ch);
+                }
+                
+                
+            }else{
+                st.push(ch);
+            }
+            
+            
+            
         }
-        return (st.size()==0);
-        
+        return st.isEmpty();
     }
 }
