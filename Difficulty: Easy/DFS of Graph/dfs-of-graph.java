a51c9af3293450aa8edfd28340cbd3469fea1f76
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-22 23:10:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.java
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.java
@@ -1,24 +1,19 @@
 class Solution {
-    public static void depth(int node, ArrayList<ArrayList<Integer>> adj, boolean[] vis, ArrayList<Integer> ans){
-        vis[node]= true;
+    public void dfs(int node,boolean [] vis,  ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans ){
+        vis[node]=true;
+        ans.add(node);
         for(int it:adj.get(node)){
-            if(vis[it]==false){
-                ans.add(it);
-                vis[it]= true;
-                depth(it, adj, vis, ans);
+            if(!vis[it]){
+                dfs(it, vis,adj, ans);
             }
         }
-        
     }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
-        int V= adj.size();
-        boolean [] vis= new boolean[V+1];
+        // code here
         
+        boolean vis[]= new boolean[adj.size()];
         ArrayList<Integer> ans= new ArrayList<>();
-        ans.add(0);
-        depth(0, adj, vis, ans);
+        dfs(0,vis, adj, ans);
         return ans;
-       
-        
     }
 }
