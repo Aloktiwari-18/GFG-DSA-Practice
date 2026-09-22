69895cfed46413b0da726b042f11eca86101df00
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-23 00:39:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,43 +1,62 @@
+class Pair{
+    int node;
+    int parent;
+    Pair(int node, int parent){
+        this.node=node;
+        this.parent=parent;
+    }
+}
+
 class Solution {
-    public static boolean dfs(int node ,int parent,  ArrayList<ArrayList<Integer>> adj, int [] vis){
-        vis[node]= 1;
-        
-        for(int it:adj.get(node)){
-            if(vis[it]==0){
-                if(dfs(it, node, adj , vis )){
+    
+    public boolean dfs(int node, int V,  ArrayList<ArrayList<Integer>> adj, boolean [] vis){
+        Queue<Pair> q= new LinkedList<>();
+        q.add(new Pair(node,-1));
+        vis[node]=true;
+        while(!q.isEmpty()){
+            int nd= q.peek().node;
+            int par= q.peek().parent;
+            q.poll();
+            
+            for(int it: adj.get(nd)){
+                if(!vis[it]){
+                    vis[it]=true;
+                    q.add(new Pair(it, nd));
+                    
+                }
+                else if(it!=par){
                     return true;
                 }
                 
+                
+                
             }
-            else if(it!=parent){
-                return true;
-            }
+            
         }
-        // pathVis[node]= 0;
         return false;
         
     }
-    public boolean isCycle(int V, int[][] edges) { // Code here
+    public boolean isCycle(int V, int[][] edges) {
         ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
-        int [] vis= new int[V];
-        // int pathVis[]= new int[V];
         for(int i=0;i<V;i++){
             adj.add(new ArrayList<>());
-        } 
-            for(int [] edge: edges){
-                adj.get(edge[0]).add(edge[1]);
-                adj.get(edge[1]).add(edge[0]);
-            }
+        }
+        for(int i=0;i<edges.length;i++){
+            int u= edges[i][0];
+            int v=edges[i][1];
             
-            for(int i=0;i<V;i++){
-                if(vis[i]==0){
-                    if(dfs(i,-1, adj, vis)){
-                        return true;
-                    }
-                    
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+            
+        }
+        // Code here
+        boolean vis[]= new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                if(dfs(i, V,adj,vis)){
+                    return true;
                 }
-            
-            
+            }
         }
         return false;
     }
