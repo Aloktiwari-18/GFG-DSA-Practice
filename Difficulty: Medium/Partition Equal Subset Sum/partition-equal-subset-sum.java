c633e4ab57180b3abf385b3ca760e8016b1ad656
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-24 00:16:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -1,53 +1,36 @@
 class Solution {
-   
-    public boolean isSame(int i, int [] arr, int sum, int [][]dp){
-
-        if(i == arr.length){
-            return sum == 0;
+    public static boolean solve(int idx, int arr[], int target, int [][]dp){
+        if(idx==arr.length){
+            return target==0;
         }
-
-        if(dp[i][sum] != -1){
-            return dp[i][sum] == 1;
+        if(dp[idx][target]!=-1){
+            return dp[idx][target]==1;
         }
-
-        boolean skip = isSame(i+1, arr, sum, dp);
-
-        boolean take = false;
-        if(sum - arr[i] >= 0){
-            take = isSame(i+1, arr, sum-arr[i], dp);
+        boolean skip= solve(idx+1, arr, target, dp);
+        boolean take=false;
+        if(target-arr[idx]>=0){
+            take= solve(idx+1, arr, target-arr[idx], dp);
         }
-
-        boolean ans = skip || take;
-
-        dp[i][sum] = ans ? 1 : 0;
-
-        return ans;
+        boolean ans= take || skip;
+          dp[idx][target] =ans ?1:0;
+          return ans;
     }
-
     static boolean equalPartition(int arr[]) {
-
-        int sum = 0;
-        int n = arr.length;
-
-        for(int i=0;i<n;i++){
-            sum += arr[i];
+        // code here
+        int sum=0;
+        for(int ele: arr){
+            sum+=ele;
         }
-
-        if(sum % 2 != 0){
+        if(sum%2!=0){
             return false;
         }
-
-        int target = sum / 2;
-
-        int dp[][] = new int[n][target+1];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=target;j++){
-                dp[i][j] = -1;
-            }
+        
+        
+        int target= sum/2;
+        int dp[][]= new int[arr.length][target+1];
+        for(int e[]:  dp){
+            Arrays.fill(e, -1);
         }
-
-        Solution obj = new Solution();
-        return obj.isSame(0, arr, target, dp);
+        return solve(0, arr, target, dp);
     }
 }
