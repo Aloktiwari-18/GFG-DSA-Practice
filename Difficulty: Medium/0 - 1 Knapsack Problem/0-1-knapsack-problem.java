2a51c59633505a317951447c1f9680376b077b8e
Alok Tiwari <151405119+Aloktiwari-18@users.noreply.github.com>
2026-09-23 23:06:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,29 +1,27 @@
 class Solution {
-    public static int calculate(int i, int W, int [] val,int []wt,int [][]dp ){
-       if(i==wt.length) return 0;
-        if(dp[i][W]!=-1){
-            return dp[i][W];
-
+    public int solve(int idx, int W, int val[], int wt[], int [][]dp){
+        if(idx==wt.length){
+            return 0;
         }
-        int skip= calculate(i+1, W, val, wt, dp);
-        if(wt[i]>W){
-            return dp[i][W]= skip;
+        if(dp[idx][W]!=-1){
+            return dp[idx][W];
         }
-        int take= val[i]+calculate(i+1, W-wt[i], val, wt, dp);
-        return dp[i][W]=Math.max(take, skip);
+        int skip= solve(idx+1, W,val, wt, dp);
+       
+        int take = 0;
+                if (W >= wt[idx]) {
+                    take = val[idx] + solve(idx + 1, W - wt[idx], val, wt, dp);
+                }
         
+         
+        return dp[idx][W]=Math.max(take, skip);
     }
     public int knapsack(int W, int val[], int wt[]) {
-        int n= wt.length;
-
-        // code here
-        int dp[][]= new int [n][W+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<W+1;j++){
-                dp[i][j]= -1;
-            }
+        int dp[][]= new int[wt.length][W+1];
+        for(int []e:dp){
+            Arrays.fill(e, -1);
         }
-        return calculate(0,W, val, wt, dp );
-        
+        // code here
+        return solve(0, W, val, wt, dp);
     }
 }
